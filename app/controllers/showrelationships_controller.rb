8f120ef53f667d84b9359747b5045a0a9041998e
chonasan <tk24120824@gmail.com>
2021-03-04 04:57:08 +0000

    secound comit

--- app/controllers/showrelationships_controller.rb
+++ app/controllers/showrelationships_controller.rb
@@ -2,22 +2,21 @@ class ShowrelationshipsController < ApplicationController
    def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    @user = User.find(params[:id])
-    redirect_to user_path(@user.id)
+    redirect_back(fallback_location: root_path)
    end
 
+    
     def destroy
       follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
       follow.destroy
-      @user = User.find(params[:id])
-      redirect_to user_path(@user.id)
+      redirect_back(fallback_location: root_path)
     end
-    
+
     private
 
     def user_params
       params.require(:user).permit(:name, :profile_image, :introduction)
     end
-    
-    
+
+
 end
