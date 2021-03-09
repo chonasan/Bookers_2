d03ffef1822a7b6c758d5b00eabfcc548a70b20e
chonasan <tk24120824@gmail.com>
2021-03-09 07:43:22 +0000

    Task6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,24 +2,30 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    comment = @book.book_comments.new(book_comment_params)
-    comment.book_id = @book.id
     @user = @book.user
     @book_comments = @book.book_comments
     @book_comment = BookComment.new
+    comment = @book.book_comments.new(book_comment_params)
+    comment.book_id = @book.id
     comment.user_id = current_user.id
     comment.save
-
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    BookComment.find(params[:id]).destroy
     @book = Book.find(params[:book_id])
     @user = @book.user
     @book_comments = @book.book_comments
     @book_comment = BookComment.new
   end
 
+#目的→手段
+#消したいコメントインスタンスを取ってくる params:id
+#showページにとってくる、Bookのインスタンス parans:book_id
+
+#〇viewでの目標
+#URLに次表示したい非同期のBookのidを入れる
+#URLに消したいコメントのidを入れる
 
   private
 
